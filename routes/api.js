f63a00ed131e3571b5fac449dc972a4c3670a3df
Prime <jeff.d.937@gmail.com>
2021-10-17 22:54:03 -0400

    'api routes'

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+router.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+
+router.post("/api/notes", (req, res) => {
+    
+      let newNote = req.body;
+      notes.push(newNote);
+        updateDb();
+    return console.log("New note: " +newNote.title);
+        });
